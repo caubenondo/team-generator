4546c6107014f2262dada9a48b92710456836616
Hai Duong <haiduong@HaiDStudio.local>
2022-06-01 00:18:26 -0700

    added easter egg

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -7,7 +7,7 @@ function writeHTML(arrayOfInstance) {
         <head>
             <meta charset="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>My Team</title>
+            <title>My HD Team</title>
             <!-- CSS only -->
             <link
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
